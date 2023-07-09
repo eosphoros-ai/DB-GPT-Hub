aa8a2950e18a6c864f0e613eed34c8a299e8902a
luchun <71970539+zhanghy-sketchzh@users.noreply.github.com>
2023-07-09 21:42:32 +0800

    Update train_qlora.py

--- src/train/train_qlora.py
+++ src/train/train_qlora.py
@@ -36,10 +36,8 @@ from peft import (
 )
 from peft.tuners.lora import LoraLayer
 from transformers.trainer_utils import PREFIX_CHECKPOINT_DIR
-os.chdir(os.getcwd())
 
 torch.backends.cuda.matmul.allow_tf32 = True
-
 logger = logging.getLogger(__name__)
 
 IGNORE_INDEX = -100
@@ -169,7 +167,7 @@ class TrainingArguments(transformers.Seq2SeqTrainingArguments):
     optim: str = field(default='paged_adamw_32bit', metadata={"help": 'The optimizer to be used'})
     per_device_train_batch_size: int = field(default=1, metadata={"help": 'The training batch size per GPU. Increase for better speed.'})
     gradient_accumulation_steps: int = field(default=16, metadata={"help": 'How many gradients to accumulate before to perform an optimizer step'})
-    max_steps: int = field(default=10, metadata={"help": 'How many optimizer update steps to take'})
+    max_steps: int = field(default=10000, metadata={"help": 'How many optimizer update steps to take'})
     weight_decay: float = field(default=0.0, metadata={"help": 'The L2 weight decay rate of AdamW'}) # use lora dropout instead for regularization if needed
     learning_rate: float = field(default=0.0002, metadata={"help": 'The learnign rate'})
     remove_unused_columns: bool = field(default=False, metadata={"help": 'Removed unused columns. Needed to make this codebase work.'})
@@ -178,7 +176,7 @@ class TrainingArguments(transformers.Seq2SeqTrainingArguments):
     do_train: bool = field(default=True, metadata={"help": 'To train or not to train, that is the question?'})
     lr_scheduler_type: str = field(default='constant', metadata={"help": 'Learning rate schedule. Constant a bit better than cosine, and has advantage for analysis'})
     warmup_ratio: float = field(default=0.03, metadata={"help": 'Fraction of steps to do a warmup for'})
-    logging_steps: int = field(default=2, metadata={"help": 'The frequency of update steps after which to log the loss'})
+    logging_steps: int = field(default=10, metadata={"help": 'The frequency of update steps after which to log the loss'})
     group_by_length: bool = field(default=True, metadata={"help": 'Group sequences into batches with same length. Saves memory and speeds up training considerably.'})
     save_strategy: str = field(default='steps', metadata={"help": 'When to save checkpoints'})
     save_steps: int = field(default=250, metadata={"help": 'How often to save a model'})
@@ -367,17 +365,6 @@ def smart_tokenizer_and_embedding_resize(
 
     Note: This is the unoptimized version that may make your embedding size not be divisible by 64.
     """
-    original_len: int = len(tokenizer)
-    num_added_toks: dict = {}
-    if tokenizer.bos_token is None:
-        num_added_toks['bos_token'] = "<bos>"
-    if tokenizer.bos_token is None:
-        num_added_toks['cls_token'] = "<cls>"
-    if tokenizer.bos_token is None:
-        num_added_toks['sep_token'] = "<s>"
-    if tokenizer.bos_token is None:
-        num_added_toks['mask_token'] = "<mask>"
-        
     num_new_tokens = tokenizer.add_special_tokens(special_tokens_dict)
     model.resize_token_embeddings(len(tokenizer))
     

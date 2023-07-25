62bee3bbec2ce9a16c43ab42699ff1a88c24f96b
zhanghy-sketchzh <1750410339@qq.com>
2023-07-25 15:39:57 +0800

    Support qlora fine-tuning of 1lama2

--- scripts/spider_qlora_finetune.sh
+++ scripts/spider_qlora_finetune.sh
@@ -3,26 +3,43 @@
 python src/sql_data_process.py 
 
 python src/train/train_qlora.py \
-    --output_dir ./adapter \
-    --dataset spider \
-    --do_train True \
-    --do_eval False \
-    --do_merge True \ 
-    --source_max_len 384 \
-    --target_max_len 128 \
-    --per_device_train_batch_size 4 \
-    --per_device_eval_batch_size 4 \
-    --gradient_accumulation_steps 4 \
-    --logging_steps 2 \
-    --max_steps 10 \
+    --use_auth \
+    --logging_steps 10 \
     --save_strategy steps \
     --data_seed 42 \
-    --save_steps 1000 \
+    --save_steps 500 \
     --save_total_limit 40 \
     --evaluation_strategy steps \
     --eval_dataset_size 1024 \
     --max_eval_samples 1000 \
-    --eval_steps 10 \
-    --optim paged_adamw_32bit \
-
+    --per_device_eval_batch_size 1 \
+    --max_new_tokens 32 \
+    --dataloader_num_workers 1 \
+    --group_by_length \
+    --logging_strategy steps \
+    --remove_unused_columns False \
+    --do_train \
+    --lora_r 64 \
+    --lora_alpha 16 \
+    --lora_modules all \
+    --double_quant \
+    --quant_type nf4 \
+    --bf16 \
+    --bits 4 \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type constant \
+    --gradient_checkpointing \
+    --source_max_len 16 \
+    --target_max_len 512 \
+    --per_device_train_batch_size 1 \
+    --gradient_accumulation_steps 16 \
+    --max_steps 1875 \
+    --eval_steps 187 \
+    --learning_rate 0.0002 \
+    --adam_beta2 0.999 \
+    --max_grad_norm 0.3 \
+    --lora_dropout 0.1 \
+    --weight_decay 0.0 \
+    --seed 0 \
+    
 python src/utils/merge_peft_adapters.py

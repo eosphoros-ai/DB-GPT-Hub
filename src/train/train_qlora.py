a2337a8bdca23bda34d9e751dbf212f1e25f954f
luchun <71970539+zhanghy-sketchzh@users.noreply.github.com>
2023-07-25 15:43:22 +0800

    Update train_qlora.py

--- src/train/train_qlora.py
+++ src/train/train_qlora.py
@@ -194,7 +194,7 @@ class TrainingArguments(transformers.Seq2SeqTrainingArguments):
         default='none',
         metadata={"help": "To use wandb or something else for reporting."}
     )
-    output_dir: str = field(default='./output', metadata={"help": 'The output dir for logs and checkpoints'})
+    output_dir: str = field(default='./adapter', metadata={"help": 'The output dir for logs and checkpoints'})
     optim: str = field(default='paged_adamw_32bit', metadata={"help": 'The optimizer to be used'})
     per_device_train_batch_size: int = field(default=1, metadata={"help": 'The training batch size per GPU. Increase for better speed.'})
     gradient_accumulation_steps: int = field(default=16, metadata={"help": 'How many gradients to accumulate before to perform an optimizer step'})

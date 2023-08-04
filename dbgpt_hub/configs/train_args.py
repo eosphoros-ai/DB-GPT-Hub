2be3890cd00d267cd4c150c91e354619c4333c7b
luchun <71970539+zhanghy-sketchzh@users.noreply.github.com>
2023-08-04 15:15:22 +0800

    Update train_args.py

--- dbgpt_hub/configs/train_args.py
+++ dbgpt_hub/configs/train_args.py
@@ -4,7 +4,7 @@ from transformers import TrainingArguments
 
 
 @dataclass
-class TrainingArguments(Seq2SeqTrainingArguments):
+class TrainingArguments(transformers.Seq2SeqTrainingArguments):
     cache_dir: Optional[str] = field(default=None)
     train_on_source: Optional[bool] = field(
         default=False,

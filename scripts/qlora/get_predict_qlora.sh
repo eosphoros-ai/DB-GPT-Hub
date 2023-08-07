af17ba79d45ed24c99ad24390d3afde8fc6be309
luchun <71970539+zhanghy-sketchzh@users.noreply.github.com>
2023-08-07 18:44:25 +0800

    Update get_predict_qlora.sh

--- scripts/qlora/get_predict_qlora.sh
+++ scripts/qlora/get_predict_qlora.sh
@@ -4,5 +4,5 @@ python dbgpt_hub/utils/sql_data_process.py \
     --data_filepaths data/spider/dev.json \
     --output_file dev_sql.json \
 
-CUDA_VISIBLE_DEVICES=2,3 python ./../predict_qlora.py \
+CUDA_VISIBLE_DEVICES=2,3 python predict_qlora.py \
 

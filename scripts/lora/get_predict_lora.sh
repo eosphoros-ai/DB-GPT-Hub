33f5d281fee3e374981096fd47d5a9d7fcdf7bb0
wangzaistone <zhaowanghappy@163.com>
2023-08-10 10:30:58 +0800

    when eval, add support predict none compare

--- scripts/lora/get_predict_lora.sh
+++ scripts/lora/get_predict_lora.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+
 # prepare dev data
 python dbgpt_hub/utils/sql_data_process.py \
     --data_filepaths data/spider/dev.json \
     --output_file dev_sql.json \
 
-# get lora predict 
-CUDA_VISIBLE_DEVICES=3 python ./predict_lora.py \
+start_time=`date +"%Y-%m-%d %H:%M:%S"`
+echo "[INFO] date:${start_time} "
 
+# get lora predict 
+CUDA_VISIBLE_DEVICES=3 python ./predict_lora.py 
+
+end_time=`date +"%Y-%m-%d %H:%M:%S"`
+echo "finished"
+echo "[INFO] date:${end_time} "

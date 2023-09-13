7402d2ee1d2be8b72c4088084ae15836e0ff2f37
wangzaistone <zhaowanghappy@163.com>
2023-09-13 11:24:06 +0800

    change config

--- dbgpt_hub/configs/config.py
+++ dbgpt_hub/configs/config.py
@@ -1,18 +1,18 @@
 import os
-ROOT_PATH = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+ROOT_PATH = os.path.dirname(os.path.dirname(
+    os.path.dirname(os.path.abspath(__file__))))
 # ROOT_PATH = "/root/autodl-tmp"
 
-# MODEL_PATH = ROOT_PATH
-MODEL_PATH = os.path.join(ROOT_PATH, "model")
+MODEL_PATH = "/home/model_files/codellama/"
+# MODEL_PATH = os.path.join(ROOT_PATH, "model")
 ADAPTER_PATH = os.path.join(ROOT_PATH, "adapter")
 MERGED_MODELS = os.path.join(ROOT_PATH, "merged_models")
 
-DATA_PATH = "/root/autodl-tmp/data/spider/pre_processed_data"
-OUT_DIR= "/root/autodl-tmp/codellama"
+# DATA_PATH = "/root/autodl-tmp/data/spider/pre_processed_data"
+# OUT_DIR= "/root/autodl-tmp/codellama"
 
-# DATA_PATH = os.path.join(ROOT_PATH, "data")
-# OUT_DIR = os.path.join(DATA_PATH, "out_pred")
+DATA_PATH = os.path.join(ROOT_PATH, "data")
+OUT_DIR = os.path.join(DATA_PATH, "out_pred")
 
 
-DEFAULT_FT_MODEL_NAME = "CodeLlama-7b-hf"
-
+DEFAULT_FT_MODEL_NAME = "CodeLlama-7b-Instruct-hf"

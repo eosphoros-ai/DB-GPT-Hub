947e68acb370b0b2ea83d9d5afaee98f65f89ebf
wangzaistone <zhaowanghappy@163.com>
2023-09-18 22:12:11 +0800

    refactor llm_base config parse,and change configs/config.py

--- dbgpt_hub/configs/model_args.py
+++ dbgpt_hub/configs/model_args.py
@@ -12,8 +12,101 @@ from dbgpt_hub.configs.config import (
     ADAPTER_PATH,
 )
 
-# Config this from config.py
-model_path = os.path.join(MODEL_PATH, DEFAULT_FT_MODEL_NAME)
+
+
+## constants 
+IGNORE_INDEX = -100
+DEFAULT_PAD_TOKEN = "[PAD]"
+DEFAULT_EOS_TOKEN = "</s>"
+DEFAULT_BOS_TOKEN = "<s>"
+DEFAULT_UNK_TOKEN = "<unk>"
+
+
+LOG_FILE_NAME = "trainer_log.jsonl"
+
+VALUE_HEAD_FILE_NAME = "value_head.bin"
+
+FINETUNING_ARGS_NAME = "finetuning_args.json"
+
+LAYERNORM_NAMES = ["norm", "ln_f", "ln_attn", "ln_mlp"]
+
+METHODS = ["full", "freeze", "lora"]
+
+STAGES = ["SFT", "Reward Modeling", "PPO", "DPO", "Pre-Training"]
+
+DATASET_STAGE_MAP = {
+    "SFT": "sft",
+    "Pre-Training": "pt",
+    "Reward Modeling": "rm",
+    "PPO": "sft",
+    "DPO": "rm",
+}
+
+SUPPORTED_MODELS = {
+    "LLaMA-7B": "huggyllama/llama-7b",
+    "LLaMA-13B": "huggyllama/llama-13b",
+    "LLaMA-30B": "huggyllama/llama-30b",
+    "LLaMA-65B": "huggyllama/llama-65b",
+    "LLaMA2-7B": "meta-llama/Llama-2-7b-hf",
+    "LLaMA2-13B": "meta-llama/Llama-2-13b-hf",
+    "LLaMA2-70B": "meta-llama/Llama-2-70b-hf",
+    "LLaMA2-7B-Chat": "meta-llama/Llama-2-7b-chat-hf",
+    "LLaMA2-13B-Chat": "meta-llama/Llama-2-13b-chat-hf",
+    "LLaMA2-70B-Chat": "meta-llama/Llama-2-70b-chat-hf",
+    "ChineseLLaMA2-7B": "ziqingyang/chinese-llama-2-7b",
+    "ChineseLLaMA2-13B": "ziqingyang/chinese-llama-2-13b",
+    "ChineseLLaMA2-7B-Chat": "ziqingyang/chinese-alpaca-2-7b",
+    "ChineseLLaMA2-13B-Chat": "ziqingyang/chinese-alpaca-2-13b",
+    "BLOOM-560M": "bigscience/bloom-560m",
+    "BLOOM-3B": "bigscience/bloom-3b",
+    "BLOOM-7B1": "bigscience/bloom-7b1",
+    "BLOOMZ-560M": "bigscience/bloomz-560m",
+    "BLOOMZ-3B": "bigscience/bloomz-3b",
+    "BLOOMZ-7B1-mt": "bigscience/bloomz-7b1-mt",
+    "Falcon-7B": "tiiuae/falcon-7b",
+    "Falcon-7B-Chat": "tiiuae/falcon-7b-instruct",
+    "Falcon-40B": "tiiuae/falcon-40b",
+    "Falcon-40B-Chat": "tiiuae/falcon-40b-instruct",
+    "Baichuan-7B": "baichuan-inc/Baichuan-7B",
+    "Baichuan-13B": "baichuan-inc/Baichuan-13B-Base",
+    "Baichuan-13B-Chat": "baichuan-inc/Baichuan-13B-Chat",
+    "Baichuan2-7B": "baichuan-inc/Baichuan2-7B-Base",
+    "Baichuan2-13B": "baichuan-inc/Baichuan2-13B-Base",
+    "Baichuan2-7B-Chat": "baichuan-inc/Baichuan2-7B-Chat",
+    "Baichuan2-13B-Chat": "baichuan-inc/Baichuan2-13B-Chat",
+    "InternLM-7B": "internlm/internlm-7b",
+    "InternLM-7B-Chat": "internlm/internlm-chat-7b",
+    "Qwen-7B": "Qwen/Qwen-7B",
+    "Qwen-7B-Chat": "Qwen/Qwen-7B-Chat",
+    "XVERSE-13B": "xverse/XVERSE-13B",
+    "ChatGLM2-6B-Chat": "THUDM/chatglm2-6b",
+}
+
+DEFAULT_MODULE = {
+    "LLaMA": "q_proj,v_proj",
+    "LLaMA2": "q_proj,v_proj",
+    "ChineseLLaMA2": "q_proj,v_proj",
+    "BLOOM": "query_key_value",
+    "BLOOMZ": "query_key_value",
+    "Falcon": "query_key_value",
+    "Baichuan": "W_pack",
+    "Baichuan2": "W_pack",
+    "InternLM": "q_proj,v_proj",
+    "Qwen": "c_attn",
+    "XVERSE": "q_proj,v_proj",
+    "ChatGLM2": "query_key_value",
+}
+
+DEFAULT_TEMPLATE = {
+    "LLaMA2": "llama2",
+    "ChineseLLaMA2": "llama2_zh",
+    "Baichuan": "baichuan",
+    "Baichuan2": "baichuan2",
+    "InternLM": "intern",
+    "Qwen": "chatml",
+    "ChatGLM2": "chatglm2",
+}
+
 
 
 @dataclass
--- dbgpt_hub/llm_base/config_parser.py
+++ dbgpt_hub/llm_base/config_parser.py
@@ -3,17 +3,171 @@ import json
 import torch
 import numpy as np
 import torch.nn as nn
+from dbgpt_hub.configs.config import IGNORE_INDEX
+from dbgpt_hub.llm_base.logging import get_logger
+from dbgpt_hub.configs import VALUE_HEAD_FILE_NAME,FINETUNING_ARGS_NAME
+from transformers import Seq2SeqTrainer
+from transformers.trainer import TRAINING_ARGS_NAME, WEIGHTS_NAME
+from transformers.modeling_utils import PreTrainedModel, unwrap_model,load_sharded_checkpoint
+from transformers.trainer import WEIGHTS_NAME, WEIGHTS_INDEX_NAME
+from peft import PeftModel
+from trl import PreTrainedModelWrapper
 from typing import TYPE_CHECKING, Any, Dict, List, Optional, Tuple, Union
 
-from llmtuner.extras.constants import IGNORE_INDEX
-from llmtuner.extras.logging import get_logger
-from llmtuner.tuner.core.trainer import PeftTrainer
-
 if TYPE_CHECKING:
+    from transformers import PreTrainedTokenizer, Seq2SeqTrainingArguments, TrainerState
     from transformers.trainer import PredictionOutput
+    from dbgpt_hub.configs.model_args  import FinetuningArguments
+
 
 
 logger = get_logger(__name__)
+
+
+def get_state_dict(model: torch.nn.Module) -> Dict[str, torch.Tensor]:
+    state_dict: Dict[str, torch.Tensor] = model.state_dict()
+    filtered_state_dict = {}
+
+    for k, v in model.named_parameters():
+        if v.requires_grad:
+            filtered_state_dict[k] = state_dict[k].cpu().clone().detach()
+
+    return filtered_state_dict
+
+
+def load_trainable_params(model: torch.nn.Module, checkpoint_dir: os.PathLike) -> bool:
+    weights_file = os.path.join(checkpoint_dir, WEIGHTS_NAME)
+    if os.path.exists(weights_file):
+        model_state_dict = torch.load(weights_file, map_location="cpu")
+        model.load_state_dict(model_state_dict, strict=False)  # skip missing keys
+    elif os.path.exists(os.path.join(checkpoint_dir, WEIGHTS_INDEX_NAME)):
+        load_sharded_checkpoint(model, checkpoint_dir, strict=False)
+    else:
+        logger.warning(
+            "Provided path ({}) does not contain pre-trained weights.".format(
+                checkpoint_dir
+            )
+        )
+        return False
+    return True
+
+
+
+class PeftModelMixin:
+    r"""
+    Patches the save and load methods in Hugging Face Trainer for PeftModel and ModelWithValueHead.
+    """
+
+    def __init__(self) -> None:  # for type checking
+        self.model: PreTrainedModel = None
+        self.tokenizer: "PreTrainedTokenizer" = None
+        self.args: "Seq2SeqTrainingArguments" = None
+        self.finetuning_args: "FinetuningArguments" = None
+        self.state: "TrainerState" = None
+        raise AssertionError("Mixin should not be initialized.")
+
+    def _save(
+        self,
+        output_dir: Optional[str] = None,
+        state_dict: Optional[Dict[str, torch.Tensor]] = None,
+    ) -> None:
+        r"""
+        Saves trainable parameters as model checkpoint.
+
+        This function will only be executed at the process zero.
+
+        Subclass and override to inject custom behavior. It should not be directly used by external scripts.
+        """
+        output_dir = output_dir if output_dir is not None else self.args.output_dir
+        os.makedirs(output_dir, exist_ok=True)
+        logger.info(f"Saving model checkpoint to {output_dir}")
+
+        model = unwrap_model(self.model)
+        if isinstance(model, PreTrainedModelWrapper):
+            # Custom state dict: https://github.com/lvwerra/trl/blob/v0.4.7/trl/models/modeling_value_head.py#L200
+            model_state_dict = state_dict or model.state_dict()
+            v_head_state_dict = {
+                name.replace("v_head.", ""): model_state_dict[name]
+                .cpu()
+                .clone()
+                .detach()
+                for name in model_state_dict.keys()
+                if name.startswith("v_head.")
+            }
+
+            torch.save(
+                v_head_state_dict, os.path.join(output_dir, VALUE_HEAD_FILE_NAME)
+            )
+            model = model.pretrained_model
+
+        state_dict = state_dict or get_state_dict(model)
+        if isinstance(model, (PeftModel, PreTrainedModel)):
+            model.config.use_cache = True
+            model.save_pretrained(
+                output_dir,
+                state_dict=state_dict,
+                safe_serialization=self.args.save_safetensors,
+            )
+            model.config.use_cache = False
+        else:
+            torch.save(state_dict, os.path.join(output_dir, WEIGHTS_NAME))
+
+        if (
+            self.finetuning_args.finetuning_type == "full"
+            and self.tokenizer is not None
+        ):
+            try:
+                self.tokenizer.save_pretrained(output_dir)
+            except:
+                logger.warning("Cannot save tokenizer, copy the files manually.")
+
+        with open(
+            os.path.join(output_dir, TRAINING_ARGS_NAME), "w", encoding="utf-8"
+        ) as f:
+            f.write(self.args.to_json_string() + "\n")
+
+        self.finetuning_args.save_to_json(
+            os.path.join(output_dir, FINETUNING_ARGS_NAME)
+        )
+
+    def _load_best_model(self):
+        r"""
+        Loads trainable parameters from model checkpoint.
+
+        Subclass and override to inject custom behavior. It should not be directly used by external scripts.
+        """
+        logger.info(
+            f"Loading best model from {self.state.best_model_checkpoint} (score: {self.state.best_metric})."
+        )
+        model = unwrap_model(self.model)
+
+        if isinstance(model, PreTrainedModelWrapper):
+            model.v_head.load_state_dict(
+                torch.load(
+                    os.path.join(
+                        self.state.best_model_checkpoint, VALUE_HEAD_FILE_NAME
+                    ),
+                    map_location="cpu",
+                )
+            )
+            model = model.pretrained_model
+
+        if isinstance(model, PeftModel):
+            model.load_adapter(self.state.best_model_checkpoint, model.active_adapter)
+        else:  # freeze/full-tuning
+            load_trainable_params(model, self.state.best_model_checkpoint)
+
+
+
+class PeftTrainer(PeftModelMixin, Seq2SeqTrainer):
+    r"""
+    Inherits Seq2SeqTrainer to support parameter-efficient checkpoints.
+    """
+
+    def __init__(self, finetuning_args: "FinetuningArguments", **kwargs):
+        Seq2SeqTrainer.__init__(self, **kwargs)
+        self.finetuning_args = finetuning_args
+
 
 
 class Seq2SeqPeftTrainer(PeftTrainer):

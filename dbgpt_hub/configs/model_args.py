cf829e9e91dd41dcc9c93bf1c53749c6816d2dbe
wangzaistone <zhaowanghappy@163.com>
2023-09-18 18:58:40 +0800

    add FinetuningArguments,The parameters related to fine-tuning the Perft module, including the hyperparameter settings for LoRA, QLoRA, etc.

--- dbgpt_hub/configs/model_args.py
+++ dbgpt_hub/configs/model_args.py
@@ -1,7 +1,7 @@
 import os
 import yaml
 from dataclasses import dataclass, field, asdict
-from typing import Optional, Any, Dict, List
+from typing import Optional, Any, Dict, List,Literal
 from transformers import Seq2SeqTrainingArguments
 from dbgpt_hub.configs.config import (
     MODEL_PATH,
@@ -261,48 +261,123 @@ class GenerationArguments:
 
 
 @dataclass
-class LoraArguments:
-    #  Number of columns of matrix A and number of rows of matrix B in Lora
-    lora_r: int = field(default=64, metadata={"help": "Lora R dimension."})
-    # Scaling factor
-    lora_alpha: float = field(default=16, metadata={"help": " Lora alpha."})
-    lora_dropout: float = field(default=0.0, metadata={"help": "Lora dropout."})
-    # Size of memory available on each GPU, in MB. The default is 40GB1 for the high-end version of the A100
-    max_memory_MB: int = field(default=40960, metadata={"help": "Free memory per gpu."})
-    lora_weight_path: str = ""
-    bias: str = "none"
-
-
-@dataclass
-class QuantArguments:
-    # With 8-bit adam, can you adjust to LION or Sophia, and even deepspeed offers multiple 1-bit optimizer options0
-    adam8bit: bool = field(default=False, metadata={"help": "Use 8-bit adam."})
-    # Whether to use quadratic quantization
-    double_quant: bool = field(
+class FinetuningArguments:
+    r"""
+    Arguments pertaining to which techniques we are going to fine-tuning with.
+    """
+    finetuning_type: Optional[Literal["lora", "freeze", "full", "none"]] = field(
+        default="lora", metadata={"help": "Which fine-tuning method to use."}
+    )
+    num_hidden_layers: Optional[int] = field(
+        default=32,
+        metadata={
+            "help": 'Number of decoder blocks in the model for partial-parameter (freeze) fine-tuning. \
+                  LLaMA choices: ["32", "40", "60", "80"], \
+                  LLaMA-2 choices: ["32", "40", "80"], \
+                  BLOOM choices: ["24", "30", "70"], \
+                  Falcon choices: ["32", "60"], \
+                  Baichuan choices: ["32", "40"] \
+                  Qwen choices: ["32"], \
+                  XVERSE choices: ["40"], \
+                  ChatGLM2 choices: ["28"]'
+        },
+    )
+    num_layer_trainable: Optional[int] = field(
+        default=3,
+        metadata={
+            "help": "Number of trainable layers for partial-parameter (freeze) fine-tuning."
+        },
+    )
+    name_module_trainable: Optional[
+        Literal["mlp", "self_attn", "self_attention"]
+    ] = field(
+        default="mlp",
+        metadata={
+            "help": 'Name of trainable modules for partial-parameter (freeze) fine-tuning. \
+                  LLaMA choices: ["mlp", "self_attn"], \
+                  BLOOM & Falcon & ChatGLM2 choices: ["mlp", "self_attention"], \
+                  Baichuan choices: ["mlp", "self_attn"], \
+                  Qwen choices: ["mlp", "attn"], \
+                  LLaMA-2, InternLM, XVERSE choices: the same as LLaMA.'
+        },
+    )
+    lora_rank: Optional[int] = field(
+        default=8, metadata={"help": "The intrinsic dimension for LoRA fine-tuning."}
+    )
+    lora_alpha: Optional[float] = field(
+        default=32.0,
+        metadata={
+            "help": "The scale factor for LoRA fine-tuning (similar with the learning rate)."
+        },
+    )
+    lora_dropout: Optional[float] = field(
+        default=0.1, metadata={"help": "Dropout rate for the LoRA fine-tuning."}
+    )
+    lora_target: Optional[str] = field(
+        default=None,
+        metadata={
+            "help": 'Name(s) of target modules to apply LoRA. Use commas to separate multiple modules. \
+                  LLaMA choices: ["q_proj", "k_proj", "v_proj", "o_proj", "gate_proj", "up_proj", "down_proj"], \
+                  BLOOM & Falcon & ChatGLM2 choices: ["query_key_value", "self_attention.dense", "mlp.dense"], \
+                  Baichuan choices: ["W_pack", "o_proj", "gate_proj", "up_proj", "down_proj"], \
+                  Qwen choices: ["c_attn", "attn.c_proj", "w1", "w2", "mlp.c_proj"], \
+                  LLaMA-2, InternLM, XVERSE choices: the same as LLaMA.'
+        },
+    )
+    resume_lora_training: Optional[bool] = field(
         default=True,
         metadata={
-            "help": "Compress the quantization statistics through double quantization."
+            "help": "Whether to resume training from the last LoRA weights or create new weights after merging them."
         },
     )
-    # Quantization type, you can choose fp4 or nf4
-    quant_type: str = field(
-        default="nf4",
-        metadata={
-            "help": "Quantization data type to use. Should be one of `fp4` or `nf4`."
-        },
+    ppo_score_norm: Optional[bool] = field(
+        default=False, metadata={"help": "Use score normalization in PPO Training."}
     )
-    # Bit width used, default is 4.
-    bits: int = field(default=4, metadata={"help": "How many bits to use."})
+    dpo_beta: Optional[float] = field(
+        default=0.1, metadata={"help": "The beta parameter for the DPO loss."}
+    )
 
     def __post_init__(self):
-        if self.bits is not None:
-            assert self.bits in [4, 8], "We only accept 4-bit or 8-bit quantization."
+        if isinstance(
+            self.lora_target, str
+        ):  # support custom target modules/layers of LoRA
+            self.lora_target = [
+                target.strip() for target in self.lora_target.split(",")
+            ]
 
-        if self.quant_type is not None:
-            assert self.quant_type in [
-                "nf4",
-                "fp4",
-            ], "We only accept `nf4` or `fp4` quantization type."
+        if (
+            self.num_layer_trainable > 0
+        ):  # fine-tuning the last n layers if num_layer_trainable > 0
+            trainable_layer_ids = [
+                self.num_hidden_layers - k - 1 for k in range(self.num_layer_trainable)
+            ]
+        else:  # fine-tuning the first n layers if num_layer_trainable < 0
+            trainable_layer_ids = [k for k in range(-self.num_layer_trainable)]
+
+        self.trainable_layers = [
+            "{:d}.{}".format(idx, self.name_module_trainable)
+            for idx in trainable_layer_ids
+        ]
+
+        assert self.finetuning_type in [
+            "lora",
+            "freeze",
+            "full",
+            "none",
+        ], "Invalid fine-tuning method."
+
+    def save_to_json(self, json_path: str):
+        r"""Saves the content of this instance in JSON format inside `json_path`."""
+        json_string = json.dumps(asdict(self), indent=2, sort_keys=True) + "\n"
+        with open(json_path, "w", encoding="utf-8") as f:
+            f.write(json_string)
+
+    @classmethod
+    def load_from_json(cls, json_path: str):
+        r"""Creates an instance from the content of `json_path`."""
+        with open(json_path, "r", encoding="utf-8") as f:
+            text = f.read()
+        return cls(**json.loads(text))
 
 
 @dataclass

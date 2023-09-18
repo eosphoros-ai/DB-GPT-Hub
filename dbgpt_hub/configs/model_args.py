02cb290bcb7447503d5339bb6813503d1b2321c4
wangzaistone <zhaowanghappy@163.com>
2023-09-18 19:08:44 +0800

    change ModelArguments,support better

--- dbgpt_hub/configs/model_args.py
+++ dbgpt_hub/configs/model_args.py
@@ -1,5 +1,6 @@
 import os
 import yaml
+import torch
 from dataclasses import dataclass, field, asdict
 from typing import Optional, Any, Dict, List,Literal
 from transformers import Seq2SeqTrainingArguments
@@ -16,37 +17,107 @@ model_path = os.path.join(MODEL_PATH, DEFAULT_FT_MODEL_NAME)
 
 @dataclass
 class ModelArguments:
-    model_name_or_path: Optional[str] = field(
-        default=model_path,
+    r"""
+    Arguments pertaining to which model/config/tokenizer we are going to fine-tune.
+    """
+    model_name_or_path: str = field(
         metadata={
-            "help": (
-                "The model checkpoint for weights initialization. Don't set if you want to\
-              train a model from scratch."
-            )
-        },
+            "help": "Path to pretrained model or model identifier from huggingface.co/models."
+        }
     )
-    tokenizer_name: Optional[str] = field(
+    cache_dir: Optional[str] = field(
         default=None,
         metadata={
-            "help": "Pretrained tokenizer name or path if not the same as model_name"
+            "help": "Where to store the pretrained models downloaded from huggingface.co."
         },
     )
-    model_revision: str = field(
+    use_fast_tokenizer: Optional[bool] = field(
+        default=False,
+        metadata={
+            "help": "Whether to use one of the fast tokenizer (backed by the tokenizers library) or not."
+        },
+    )
+    use_auth_token: Optional[bool] = field(
+        default=False,
+        metadata={
+            "help": "Will use the token generated when running `huggingface-cli login`."
+        },
+    )
+    model_revision: Optional[str] = field(
         default="main",
         metadata={
             "help": "The specific model version to use (can be a branch name, tag name or commit id)."
         },
     )
-    trust_remote_code: Optional[bool] = field(
-        default=False,
+    padding_side: Optional[Literal["left", "right"]] = field(
+        default="left",
+        metadata={"help": "The side on which the model should have padding applied."},
+    )
+    quantization_bit: Optional[int] = field(
+        default=None, metadata={"help": "The number of bits to quantize the model."}
+    )
+    quantization_type: Optional[Literal["fp4", "nf4"]] = field(
+        default="nf4",
+        metadata={"help": "Quantization data type to use in int4 training."},
+    )
+    double_quantization: Optional[bool] = field(
+        default=True,
         metadata={
-            "help": "Enable unpickling of arbitrary code in AutoModelForCausalLM#from_pretrained."
+            "help": "Whether to use double quantization in int4 training or not."
         },
     )
-    use_auth_token: Optional[bool] = field(
-        default=False,
-        metadata={"help": "Enables using Huggingface auth token from Git Credentials."},
+    rope_scaling: Optional[Literal["linear", "dynamic"]] = field(
+        default=None, metadata={"help": "Adopt scaled rotary positional embeddings."}
     )
+    checkpoint_dir: Optional[str] = field(
+        default=None,
+        metadata={
+            "help": "Path to the directory(s) containing the delta model checkpoints as well as the configurations."
+        },
+    )
+    # reward_model: Optional[str] = field(
+    #     default=None,
+    #     metadata={"help": "Path to the directory containing the checkpoints of the reward model."}
+    # )
+    plot_loss: Optional[bool] = field(
+        default=False,
+        metadata={
+            "help": "Whether to plot the training loss after fine-tuning or not."
+        },
+    )
+    hf_auth_token: Optional[str] = field(
+        default=None, metadata={"help": "Auth token to log in with Hugging Face Hub."}
+    )
+    compute_dtype: Optional[torch.dtype] = field(
+        default=None,
+        metadata={
+            "help": "Used in quantization configs. Do not specify this argument manually."
+        },
+    )
+    model_max_length: Optional[int] = field(
+        default=None,
+        metadata={
+            "help": "Used in rope scaling. Do not specify this argument manually."
+        },
+    )
+
+    def __post_init__(self):
+        if self.compute_dtype is not None or self.model_max_length is not None:
+            raise ValueError("These arguments cannot be specified.")
+
+        if self.checkpoint_dir is not None:  # support merging multiple lora weights
+            self.checkpoint_dir = [cd.strip() for cd in self.checkpoint_dir.split(",")]
+
+        if self.quantization_bit is not None:
+            assert self.quantization_bit in [
+                4,
+                8,
+            ], "We only accept 4-bit or 8-bit quantization."
+
+        if self.use_auth_token == True and self.hf_auth_token is not None:
+            from huggingface_hub.hf_api import HfFolder  # lazy load
+
+            HfFolder.save_token(self.hf_auth_token)
 
 
 @dataclass

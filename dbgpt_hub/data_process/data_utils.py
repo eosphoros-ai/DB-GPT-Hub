89b69b172d69dfe6c6f0b6b32c31476848716ccd
wangzaistone <zhaowanghappy@163.com>
2023-09-19 17:02:22 +0800

    refactor data sparse func,delete data_module.py

--- dbgpt_hub/data_process/data_utils.py
+++ dbgpt_hub/data_process/data_utils.py
@@ -5,6 +5,11 @@ import pandas as pd
 from typing import Any, Dict, List, Optional, Tuple, Union
 from datasets import Dataset, DatasetDict, concatenate_datasets, load_dataset
 from dbgpt_hub.configs.data_args import DEFAULT_PROMPT_DICT,ALPACA_PROMPT_DICT,SQL_PROMPT_DICT
+from transformers.tokenization_utils import PreTrainedTokenizer
+
+from .data_utils import make_data_module
+from .sft_dataset import DataCollatorForSupervisedDataset, SFTInstructionDataset
+
 
 
 
@@ -353,3 +358,46 @@ def make_data_module(args):
         f"Concatenated dataset list: {dataset_name_list}, #eval dataset size: {len(concate_eval)}"
     ) if concate_eval else None
     return concate_train, concate_eval, mutliturn_lst[0]
+
+
+def make_supervised_data_module(tokenizer: PreTrainedTokenizer, args):
+    train_dataset, eval_dataset, multi_turn = make_data_module(args)
+    max_seq_length = tokenizer.model_max_length
+
+    train_dataset = (
+        SFTInstructionDataset(
+            train_dataset,
+            tokenizer=tokenizer,
+            max_seq_len=max_seq_length,
+        )
+        if args.do_train
+        else None
+    )
+
+    eval_dataset = (
+        SFTInstructionDataset(
+            eval_dataset,
+            tokenizer=tokenizer,
+            max_seq_len=max_seq_length,
+        )
+        if args.do_eval
+        else None
+    )
+
+    print(
+        f"train_dataset: {type(train_dataset)}, mutlti-turn: {multi_turn},  #length: {len(train_dataset)}"
+    ) if args.do_train else None
+    print(
+        f"eval_dataset: {type(eval_dataset)}, mutlti-turn: {multi_turn}, #length: {len(eval_dataset)}"
+    ) if args.do_eval else None
+
+    print("Adding data collator: ", DataCollatorForSupervisedDataset)
+    data_collator = DataCollatorForSupervisedDataset(
+        tokenizer=tokenizer, predict_with_generate=args.predict_with_generate
+    )
+
+    return {
+        "train_dataset": train_dataset,
+        "eval_dataset": eval_dataset,
+        "data_collator": data_collator,
+    }

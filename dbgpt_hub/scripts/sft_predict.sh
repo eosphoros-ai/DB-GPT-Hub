5b06b4292a514ab4db32e643c2211bdd2c4ee184
Healthyshi <18811355006@163.com>
2023-09-24 22:32:19 +0800

    modify the path of llm and checkpoint

--- dbgpt_hub/scripts/sft_predict.sh
+++ dbgpt_hub/scripts/sft_predict.sh
@@ -1,5 +1,5 @@
 CUDA_VISIBLE_DEVICES=0,1  python dbgpt_hub/predict/predict.py \
-    --model_name_or_path /home/pci/data/cpf/Lenovo/LLM_checkpoint/Llama-2-13b-chat-hf \
+    --model_name_or_path Llama-2-13b-chat-hf \
     --template llama2 \
     --finetuning_type lora \
-    --checkpoint_dir /home/pci/data/cpf/Lenovo/DB-GPT-Hub/dbgpt_hub/output/adapter
+    --checkpoint_dir dbgpt_hub/output/adapter

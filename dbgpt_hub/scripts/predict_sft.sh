6849384b4e42f333fa4dbd32465ddc4b01a026e3
wangzaistone <zhaowanghappy@163.com>
2023-09-25 17:48:42 +0800

    test train,pred run succ,base model baichuan2,llama2

--- dbgpt_hub/scripts/predict_sft.sh
+++ dbgpt_hub/scripts/predict_sft.sh
@@ -11,5 +11,12 @@ CUDA_VISIBLE_DEVICES=0 python dbgpt_hub/predict/predict.py \
     --template baichuan2_eval \
     --quantization_bit 4 \
     --finetuning_type lora \
-    --checkpoint_dir dbgpt_hub/output/adapter
+    --checkpoint_dir dbgpt_hub/output/adapter/baichuan2-13b-qlora 
 
+
+## wangzai codellama2_pred test a100
+# CUDA_VISIBLE_DEVICES=0,1  python dbgpt_hub/predict/predict.py \
+#     --model_name_or_path /home/model_files/codellama/CodeLlama-7b-Instruct-hf \
+#     --template llama2 \
+#     --finetuning_type lora \
+#     --checkpoint_dir dbgpt_hub/output/adapter/code_llama_7b-qlora
--- dbgpt_hub/scripts/train_sft.sh
+++ dbgpt_hub/scripts/train_sft.sh
@@ -1,6 +1,6 @@
 wandb offline
-
-deepspeed --num_gpus 1 --master_port=9945 dbgpt_hub/train/sft_train.py \
+# v100
+CUDA_VISIBLE_DEVICES=0 python dbgpt_hub/train/sft_train.py \
     --deepspeed dbgpt_hub/configs/ds_config.json \
     --quantization_bit 4 \
     --model_name_or_path /home/model/Baichuan2-13B-Chat \
@@ -10,18 +10,18 @@ deepspeed --num_gpus 1 --master_port=9945 dbgpt_hub/train/sft_train.py \
     --max_target_length 512 \
     --template baichuan2 \
     --finetuning_type lora \
-    --lora_rank 8 \
-    --lora_alpha 32 \
+    --lora_rank 32 \
+    --lora_alpha 8 \
     --lora_target W_pack \
-    --output_dir dbgpt_hub/output/adapter \
+    --output_dir dbgpt_hub/output/adapter/baichuan2-13b-qlora \
     --overwrite_cache \
     --overwrite_output_dir \
     --per_device_train_batch_size 1 \
     --gradient_accumulation_steps 4 \
     --lr_scheduler_type cosine_with_restarts \
     --logging_steps 10 \
-    --save_steps 30 \
+    --save_steps 500 \
     --learning_rate 5e-5 \
-    --num_train_epochs 0.2 \
+    --num_train_epochs 2 \
     --plot_loss 
     # --bf16#v100不支持bf16

9223454098f863d16e128ecada65e98ee4a78df7
Healthyshi <18811355006@163.com>
2023-11-01 15:49:22 +0800

    Create vllm_chat_model.py
    support for vllm predict

---
+++ dbgpt_hub/llm_base/vllm_chat_model.py
@@ -0,0 +1,139 @@
+import torch
+import json
+from typing import Any, Dict, Generator, List, Optional, Tuple
+from threading import Thread
+from transformers import GenerationConfig, TextIteratorStreamer, AutoTokenizer
+from vllm import LLM, SamplingParams
+
+from dbgpt_hub.llm_base.config_parser import get_infer_args
+from dbgpt_hub.llm_base.load_tokenizer import dispatch_model, load_model_and_tokenizer
+from dbgpt_hub.llm_base.model_trainer import get_logits_processor
+from dbgpt_hub.data_process.data_utils import get_template_and_fix_tokenizer
+
+
+class ChatModel:
+    def __init__(self, args: Optional[Dict[str, Any]] = None) -> None:
+        (
+            model_args,
+            self.data_args,
+            finetuning_args,
+            self.generating_args,
+        ) = get_infer_args(args)
+
+        self.model = LLM(
+            model=model_args.model_name_or_path, 
+            tensor_parallel_size=4,
+            trust_remote_code=True,
+            dtype="float16",
+            max_model_len=8192
+            )
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            model_args.model_name_or_path,
+            use_fast=False,
+            trust_remote_code=True
+        )
+        self.tokenizer.padding_side = "left"
+        # self.model = dispatch_model(self.model)
+        self.template = get_template_and_fix_tokenizer(
+            self.data_args.template, self.tokenizer
+        )
+        self.system_prompt = self.data_args.system_prompt
+
+    def process_args(
+        self,
+        query: str,
+        history: Optional[List[Tuple[str, str]]] = None,
+        system: Optional[str] = None,
+        **input_kwargs
+    ) -> Tuple[Dict[str, Any], int]:
+        system = system or self.system_prompt
+
+        prompt, _ = self.template.encode_oneturn(
+            tokenizer=self.tokenizer,
+            query=query,
+            resp="",
+            history=history,
+            system=system,
+        )
+        input_ids = self.tokenizer.decode(torch.tensor(prompt))
+       
+        do_sample = input_kwargs.pop("do_sample", None)
+        temperature = input_kwargs.pop("temperature", None)
+        top_p = input_kwargs.pop("top_p", None)
+        top_k = input_kwargs.pop("top_k", None)
+        repetition_penalty = input_kwargs.pop("repetition_penalty", None)
+        max_length = input_kwargs.pop("max_length", None)
+        max_new_tokens = input_kwargs.pop("max_new_tokens", None)
+
+        # generating_args = self.generating_args.to_dict()
+        # generating_args.update(
+        #     dict(
+        #         do_sample=do_sample
+        #         if do_sample is not None
+        #         else generating_args["do_sample"],
+        #         temperature=temperature or generating_args["temperature"],
+        #         top_p=top_p or generating_args["top_p"],
+        #         top_k=top_k or generating_args["top_k"],
+        #         repetition_penalty=repetition_penalty
+        #         or generating_args["repetition_penalty"],
+        #         eos_token_id=[self.tokenizer.eos_token_id]
+        #         + self.tokenizer.additional_special_tokens_ids,
+        #         pad_token_id=self.tokenizer.pad_token_id,
+        #     )
+        # )
+
+        # if max_length:
+        #     generating_args.pop("max_new_tokens", None)
+        #     generating_args["max_length"] = max_length
+
+        # if max_new_tokens:
+        #     generating_args.pop("max_length", None)
+        #     generating_args["max_new_tokens"] = max_new_tokens
+
+        # gen_kwargs = dict(
+        #     inputs=input_ids,
+        #     generation_config=GenerationConfig(**generating_args),
+        #     logits_processor=get_logits_processor(),
+        # )
+
+        samplingparams = SamplingParams(temperature=0.95)
+
+        return input_ids, samplingparams
+
+    @torch.inference_mode()
+    def chat(
+        self,
+        query: str,
+        history: Optional[List[Tuple[str, str]]] = None,
+        system: Optional[str] = None,
+        **input_kwargs
+    ) -> Tuple[str, Tuple[int, int]]:
+        input_ids, sampling_params = self.process_args(
+            query, history, system, **input_kwargs
+        )
+
+        print(input_ids)
+        outputs = self.model.generate(prompts=input_ids, sampling_params=sampling_params)
+        for output in outputs:
+            prompt = output.prompt
+            generated_text = output.outputs[0].text
+        return generated_text
+
+    @torch.inference_mode()
+    def stream_chat(
+        self,
+        query: str,
+        history: Optional[List[Tuple[str, str]]] = None,
+        system: Optional[str] = None,
+        **input_kwargs
+    ) -> Generator[str, None, None]:
+        gen_kwargs, _ = self.process_args(query, history, system, **input_kwargs)
+        streamer = TextIteratorStreamer(
+            self.tokenizer, timeout=60.0, skip_prompt=True, skip_special_tokens=True
+        )
+        gen_kwargs["streamer"] = streamer
+
+        thread = Thread(target=self.model.generate, kwargs=gen_kwargs)
+        thread.start()
+
+        yield from streamer

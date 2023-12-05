f91a02b6ded4591601a0ddfa30d08e32304f6bfa
junewgl <45283002+junewgl@users.noreply.github.com>
2023-12-05 12:14:22 +0800

    feat: add API interface for model baseline result (#162)

---
+++ dbgpt_hub/baseline/show_result_api.py
@@ -0,0 +1,34 @@
+from typing import Optional, Dict, Any
+
+from dbgpt_hub.baseline import show_result
+
+
+def show_all():
+    show_result.show_all_api()
+
+
+def show_model(args: Optional[Dict[str, Any]] = None):
+    # Arguments for show result
+    if args is None:
+        args = {
+            "dataset": "spider",
+            "model": "llama2-7b-hf",
+            "sft": "lora",
+            "prompt": "alpaca",
+        }
+    else:
+        args = args
+
+    show_result.show_model_api(args)
+
+
+if __name__ == "__main__":
+    show_all()
+
+    show_args = {
+        "dataset": "spider",
+        "model": "llama2-7b-hf",
+        "method": "lora",
+        "prompt": "alpaca",
+    }
+    show_model(show_args)

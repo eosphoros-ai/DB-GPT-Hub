b9b087cbc7298fe0fe46adcdc3e52c8c5d076192
junewgl <1965259211@qq.com>
2023-12-05 11:36:29 +0800

    style: run black for model baseline result

--- dbgpt_hub/baseline/show_result.py
+++ dbgpt_hub/baseline/show_result.py
@@ -10,12 +10,13 @@ sys.path.append(ROOT_PATH)
 
 baseline_file = "./dbgpt_hub/baseline/baseline.json"
 # read json
-with open(baseline_file, 'r') as file:
+with open(baseline_file, "r") as file:
     baseline_json = json.load(file)
+
 
 def print_models_info(dataset, model, method, prompt):
     print_table_models = PrettyTable()
-    models_header = ['dataset', 'model', 'method', 'prompt']
+    models_header = ["dataset", "model", "method", "prompt"]
     models_info = [dataset, model, method, prompt]
     print_table_models.field_names = models_header
     print_table_models.add_rows([models_info])
@@ -24,7 +25,7 @@ def print_models_info(dataset, model, method, prompt):
 
 def print_scores_info(acc_data):
     print_table_scores = PrettyTable()
-    scores_header = ['etype', 'easy', 'medium', 'hard', 'extra', 'all']
+    scores_header = ["etype", "easy", "medium", "hard", "extra", "all"]
     print_table_scores.field_names = scores_header
     eytpe = "ex"
     ex_score = [acc_data[eytpe][key] for key in acc_data[eytpe].keys()]
@@ -32,23 +33,13 @@ def print_scores_info(acc_data):
     eytpe = "em"
     em_score = [acc_data[eytpe][key] for key in acc_data[eytpe].keys()]
     em_score.insert(0, eytpe)
-    print_table_scores.add_rows(
-        [
-            ex_score,
-            em_score 
-        ]
-    )
+    print_table_scores.add_rows([ex_score, em_score])
     return print_table_scores
 
-def show_model(
-    dataset,
-    model,
-    method,
-    prompt
-    ):
 
+def show_model(dataset, model, method, prompt):
     # 1.get res
-    acc_data = baseline_json[dataset][model][method][prompt]['acc']
+    acc_data = baseline_json[dataset][model][method][prompt]["acc"]
 
     # 2.print models info
     print_table_models = print_models_info(dataset, model, method, prompt)
@@ -58,18 +49,15 @@ def show_model(
     print_table_scores = print_scores_info(acc_data)
     print(print_table_scores)
 
+
 def show_model_api(args: Optional[Dict[str, Any]] = None):
     dataset = args["dataset"]
     model = args["model"]
     method = args["method"]
     prompt = args["prompt"]
 
-    show_model(
-        dataset, 
-        model, 
-        method, 
-        prompt
-    )
+    show_model(dataset, model, method, prompt)
+
 
 def show_all():
     datasets = baseline_json.keys()
@@ -81,10 +69,12 @@ def show_all():
                 prompts = baseline_json[dataset][model][method].keys()
                 for prompt in prompts:
                     # 1.get scores info
-                    acc_data = baseline_json[dataset][model][method][prompt]['acc']
+                    acc_data = baseline_json[dataset][model][method][prompt]["acc"]
 
                     # 2.print models info
-                    print_table_models = print_models_info(dataset, model, method, prompt)
+                    print_table_models = print_models_info(
+                        dataset, model, method, prompt
+                    )
                     print(print_table_models)
 
                     # 3.print scores info
@@ -92,27 +82,23 @@ def show_all():
                     print(print_table_scores)
 
 
-
-            
 def show_all_api():
-   show_all()
-
+    show_all()
 
 
 # def update():
 #     # todo : 更新baseline.json
-#     #  
+#     #
 
 
 if __name__ == "__main__":
     # args
     show_args = {
-        "dataset" : "spider",
-        "model" : "llama2-7b-hf",
-        "method" : "lora",
-        "prompt" : "alpaca",
+        "dataset": "spider",
+        "model": "llama2-7b-hf",
+        "method": "lora",
+        "prompt": "alpaca",
     }
     show_model(show_args)
-    
+
     show_all()
-    
--- dbgpt_hub/baseline/show_result_api.py
+++ dbgpt_hub/baseline/show_result_api.py
@@ -6,16 +6,15 @@ from dbgpt_hub.baseline import show_result
 def show_all():
     show_result.show_all_api()
 
-def show_model(
-    args: Optional[Dict[str, Any]] = None
-):
-     # Arguments for show result
+
+def show_model(args: Optional[Dict[str, Any]] = None):
+    # Arguments for show result
     if args is None:
         args = {
-           "dataset":"spider",
-           "model":"llama2-7b-hf",
-           "sft":"lora",
-           "prompt":"alpaca",
+            "dataset": "spider",
+            "model": "llama2-7b-hf",
+            "sft": "lora",
+            "prompt": "alpaca",
         }
     else:
         args = args
@@ -23,14 +22,13 @@ def show_model(
     show_result.show_model_api(args)
 
 
-
 if __name__ == "__main__":
     show_all()
 
     show_args = {
-    "dataset" : "spider",
-    "model" : "llama2-7b-hf",
-    "method" : "lora",
-    "prompt" : "alpaca"
+        "dataset": "spider",
+        "model": "llama2-7b-hf",
+        "method": "lora",
+        "prompt": "alpaca",
     }
     show_model(show_args)

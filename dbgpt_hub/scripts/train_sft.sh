c066049d02fd186bcfaceb5cb6d3986d0880cf2f
junewgl <45283002+junewgl@users.noreply.github.com>
2023-12-28 21:27:16 +0800

    feat: add one-shot train (#197)

--- dbgpt_hub/scripts/train_sft.sh
+++ dbgpt_hub/scripts/train_sft.sh
@@ -5,11 +5,24 @@ train_log="dbgpt_hub/output/logs/train_sft_test_${current_date}.log"
 start_time=$(date +%s)
 echo " Train Start time: $(date -d @$start_time +'%Y-%m-%d %H:%M:%S')" >>${train_log}
 
+# # zero-shot
+# num_shot=0
+
+# one-shot train
+num_shot=1
+
+dataset="example_text2sql_train"
+if [ "$num_shot" -eq 1 ]; then
+    dataset="example_text2sql_train_one_shot"
+fi
+model_name_or_path="Your_download_CodeLlama-13b-Instruct-hf_path"
+output_dir="dbgpt_hub/output/adapter/CodeLlama-13b-sql-lora"
+
 # the default param set could be run in a server with one a100(40G) gpu, if your server not support the set,you can set smaller param such as  lora_rank and use qlora with quant 4 eg...
 CUDA_VISIBLE_DEVICES=0 python dbgpt_hub/train/sft_train.py \
-    --model_name_or_path Your_download_CodeLlama-13b-Instruct-hf_path \
+    --model_name_or_path $model_name_or_path \
     --do_train \
-    --dataset example_text2sql_train \
+    --dataset $dataset \
     --max_source_length 2048 \
     --max_target_length 512 \
     --finetuning_type lora \
@@ -17,7 +30,7 @@ CUDA_VISIBLE_DEVICES=0 python dbgpt_hub/train/sft_train.py \
     --template llama2 \
     --lora_rank 64 \
     --lora_alpha 32 \
-    --output_dir dbgpt_hub/output/adapter/CodeLlama-13b-sql-lora \
+    --output_dir $output_dir \
     --overwrite_cache \
     --overwrite_output_dir \
     --per_device_train_batch_size 1 \

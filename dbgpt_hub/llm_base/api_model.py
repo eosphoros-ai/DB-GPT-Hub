cce441067e3a4251f1f3c9363c81eb42ceda80a3
yeounoh <yeounoh@google.com>
2024-05-21 18:27:41 -0700

    Correct for no column error

--- dbgpt_hub/llm_base/api_model.py
+++ dbgpt_hub/llm_base/api_model.py
@@ -13,7 +13,9 @@ from vertexai.generative_models import GenerativeModel
 from google.generativeai.types import HarmCategory, HarmBlockThreshold
 
 import logging
+
 logging.basicConfig(level=logging.DEBUG)
+
 
 class GeminiModel:
 
@@ -25,78 +27,130 @@ class GeminiModel:
             self.generating_args,
         ) = get_infer_args(args)
         vertexai.init(project="400355794761", location="us-central1")
-        sft_job = sft.SupervisedTuningJob("projects/400355794761/locations/us-central1/tuningJobs/6853268654671265792")
-        self.model = GenerativeModel(model_name=sft_job.tuned_model_endpoint_name)
+        sft_job = sft.SupervisedTuningJob(
+            "projects/400355794761/locations/us-central1/tuningJobs/6853268654671265792"
+        )
+        self.model = GenerativeModel(
+            model_name=sft_job.tuned_model_endpoint_name)
         #self.model = GenerativeModel(model_name="gemini-1.0-pro-002")
         "projects/400355794761/locations/us-central1/tuningJobs/6853268654671265792"
         self.template = get_template(self.data_args.template)
         self.system_prompt = self.data_args.system_prompt
 
     def _generate_sql(self, query, temperature=0.):
-        resp = self.model.generate_content(
-                query,
-                generation_config={"temperature": temperature},
-                safety_settings={
-                    HarmCategory(0): HarmBlockThreshold.BLOCK_NONE,
-                    HarmCategory(1): HarmBlockThreshold.BLOCK_NONE,
-                    HarmCategory(2): HarmBlockThreshold.BLOCK_NONE,
-                    HarmCategory(3): HarmBlockThreshold.BLOCK_NONE,
-                    HarmCategory(4): HarmBlockThreshold.BLOCK_NONE,
-                }).text.replace("```sql","").replace("```","\n")
-        resp = re.sub('\s+',' ', resp).strip()
+        resp = self.model.generate_content(query,
+                                           generation_config={
+                                               "temperature": temperature
+                                           },
+                                           safety_settings={
+                                               HarmCategory(0):
+                                               HarmBlockThreshold.BLOCK_NONE,
+                                               HarmCategory(1):
+                                               HarmBlockThreshold.BLOCK_NONE,
+                                               HarmCategory(2):
+                                               HarmBlockThreshold.BLOCK_NONE,
+                                               HarmCategory(3):
+                                               HarmBlockThreshold.BLOCK_NONE,
+                                               HarmCategory(4):
+                                               HarmBlockThreshold.BLOCK_NONE,
+                                           }).text.replace("```sql",
+                                                           "").replace(
+                                                               "```", "\n")
+        resp = re.sub('\s+', ' ', resp).strip()
         return resp
 
     def verify_and_correct(self, query, sql, db_folder_path):
+
         def isValidSQL(sql, db):
-          conn = sqlite3.connect(db)
-          cursor = conn.cursor()
-          err = ""
-          try:
-              cursor.execute(sql)
-          except sqlite3.Error as e:
-              logging.error(e)
-              err = str(e)
-              return False, err
-          finally:
-              if conn:
-                conn.close()
-          return True, err
-        db_name = query.split("### Context:\n")[1].split(" contains tables ")[0].split("database ")[1]
+            conn = sqlite3.connect(db)
+            cursor = conn.cursor()
+            err = ""
+            try:
+                cursor.execute(sql)
+            except sqlite3.Error as e:
+                logging.error(e)
+                err = str(e)
+                return False, err
+            finally:
+                if conn:
+                    conn.close()
+            return True, err
+
+        db_name = query.split("### Context:\n")[1].split(
+            " contains tables ")[0].split("database ")[1]
         db_path = os.path.join(db_folder_path, db_name) + f"/{db_name}.sqlite"
         logging.info("Connecting to " + db_path)
 
         _sql = sql
-        retry_cnt = 0
+        retry_cnt, max_retries = 0, 2
         valid, err = isValidSQL(_sql, db_path)
 
-
-        while not valid and retry_cnt < 5:
+        while not valid and retry_cnt < max_retries:
             logging.info(f"{_sql} , failed due to {err}")
             if "no such column" in err:
-                col_name = err.split(".")[-1]
-                wrong_table_name = err.split(".")[0].split(" ")[-1]
-                wrong_table_name = _sql.split(f" AS {wrong_table_name}")[0].split(" ")[-1]
-                new_prompt = (f"Your previously generated SQL query, {_sql}, is not valid for this reason: {err}. "
-                              f"\"{wrong_table_name}\" doesn't have column \"{col_name}\". Please make sure you associate \"{col_name}\" with the correct table. \n\n")
-                logging.info("*** trying with this prompt: " + new_prompt)
-                new_prompt += f"Now try again: {query}"
+                col_name = err.split(": ")[-1]
+                col_name = col_name.split(".")[-1]
+                context_str = query[query.find("# Context:"):query.
+                                    find("#Input:")]
+                input_str = query[query.find("#Input:"):query.find("$Response:"
+                                                                   )]
+                if context_str.find(f"\"{col_name}\"") == -1:
+                    new_prompt = (
+                        f"Read this text carefully that describes the tables and their associated columns: {context_str}\n\n"
+                        f"The column name, \"{col_name}\" is not valid. "
+                        f"Find the correct column name that should have been used instead of \"{col_name}\". "
+                        "Be careful of any typo or missing spaces, prefer a correction with the minimum edit distance. "
+                        "Just return the column name and no other characters or quotations."
+                    )
+                    col_name = self._generate_sql(new_prompt)
+                new_prompt = (
+                    f"You need to identify the correct table name for the column, \"{col_name}\". "
+                    f"We can do this step-by-step. First, find all the tables that has \"{col_name}\" according to this instruction: {context_str}\n\n"
+                    "Next, if there is only one candidate, just return that table. "
+                    "Otherwise, if there are multiple candidates identified in the previous step, then return the one that is most likely given the user question {input_str}. "
+                    "Note that the final table must have the column \"{col_name}\" according to according to the above instruction. "
+                    "Always return the table name and no other chracters or quotations. "
+                )
+                table_name = self._generate_sql(new_prompt)
+                new_prompt = (
+                    f"This query, {_sql}, is not valid due to this error: {err}\n"
+                    f"We can correct this taking some steps. "
+                    f"First, understand the foreign key relationship between the tables by reading this: {context_str}\n\n"
+                    f"Second, address the error using the correct column reference, {table_name + '.`' + col_name + '`'}\n"
+                    "Beware of the table aliases in the query. "
+                    "Rewrite and correct the query, so that the column \"{col_name}\" is correctly referenced from the table \"{table_name}\", "
+                    "and just return the corrected query."
+                )
+                _sql = self._generate_sql(new_prompt)
+
+                if table_name not in _sql:
+                    new_prompt = (
+                        f"We need to use the column {table_name + '.`' + col_name + '`'} in the SQL query, {_sql}; "
+                        f"however, {table_name} is not being used in the query. Introduce the table to the query using proper joins, "
+                        f"according to this table and column and foreign key information: {context_str}\n\n"
+                        "Return the corrected query. Always return just the SQL query."
+                    )
+                    _sql = self._generate_sql(new_prompt)
+                logging.info(
+                    f" *** Correcting {err} with {table_name + '.' + col_name} ...")
             else:
-                new_prompt = (f"Your previously generated SQL query, {_sql}, is not valid for this reason: {err}. "
-                              "Here are some useful tips: "
-                              "1) Table Aliases: Use aliases to avoid duplicate table name conflicts; "
-                              "2) Column References: Verify column names and use table_name.column_name format; "
-                              "3) use the date function when comparing dates; "
-                              "4) when dealing with ratios, cast the calculated values as REAL; "
-                              "5) Table Joins: Ensure table names are correct and use appropriate joins.\n\n")
+                new_prompt = (
+                    f"Your previously generated SQL query, {_sql}, is not valid for this reason: {err}. "
+                    "Here are some useful tips: "
+                    "1) Table Aliases: Use aliases to avoid duplicate table name conflicts; "
+                    "2) Column References: Verify column names and use table_name.column_name format; "
+                    "3) use the date function when comparing dates; "
+                    "4) when dealing with ratios, cast the calculated values as REAL; "
+                    "5) Table Joins: Ensure table names are correct and use appropriate joins.\n\n"
+                )
                 new_prompt += f"Try again, and remember that  request {query}"
-            _sql = self._generate_sql(new_prompt, temperature=0.2)
+                _sql = self._generate_sql(new_prompt, temperature=0.2)
             valid, err = isValidSQL(_sql, db_path)
             retry_cnt += 1
-        if retry_cnt == 5:
+        if retry_cnt == max_retries:
             logging.info(f"Failed to generate a valid query, had {_sql}")
             return _sql
         return _sql
-
 
     @torch.inference_mode()
     def chat(self,

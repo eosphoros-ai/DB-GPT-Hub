0b2b7ed69e0593fd16195fa4850def54a793f28c
Fangyin Cheng <staneyffer@gmail.com>
2024-06-11 19:46:47 +0800

    feat(nlu): First NLU finetuning

--- src/dbgpt-hub-sql/dbgpt_hub_sql/eval/evaluation_api.py
+++ src/dbgpt-hub-sql/dbgpt_hub_sql/eval/evaluation_api.py
@@ -9,12 +9,12 @@ def start_evaluate(
     # Arguments for evaluation
     if args is None:
         args = {
-            "input": "./dbgpt_hub_sql/output/pred/pred_sql_dev_skeleton.sql",
-            "gold": "./dbgpt_hub_sql/data/eval_data/gold.txt",
-            "gold_natsql": "./dbgpt_hub_sql/data/eval_data/gold_natsql2sql.txt",
-            "db": "./dbgpt_hub_sql/data/spider/database",
-            "table": "./dbgpt_hub_sql/data/eval_data/tables.json",
-            "table_natsql": "./dbgpt_hub_sql/data/eval_data/tables_for_natsql2sql.json",
+            "input": "./dbgpt_hub/output/pred/pred_sql_dev_skeleton.sql",
+            "gold": "./dbgpt_hub/data/eval_data/gold.txt",
+            "gold_natsql": "./dbgpt_hub/data/eval_data/gold_natsql2sql.txt",
+            "db": "./dbgpt_hub/data/spider/database",
+            "table": "./dbgpt_hub/data/eval_data/tables.json",
+            "table_natsql": "./dbgpt_hub/data/eval_data/tables_for_natsql2sql.json",
             "etype": "exec",
             "plug_value": True,
             "keep_distict": False,

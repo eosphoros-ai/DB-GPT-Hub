0de038e30c97a8b9273b28d047cc30edb18db7ec
Songlin Lyu <lyusonglin990625@gmail.com>
2024-07-23 16:18:06 +0800

    create dbgpt-hub-graph

---
+++ src/dbgpt-hub-graph/dbgpt_hub_graph/data_generator/llm_generator.py
@@ -0,0 +1,23 @@
+from abc import ABC, abstractmethod
+from typing import Any, Dict, List, Optional, Tuple, Union
+
+
+class LLMGenerator(ABC):
+    """An interface for large language model data generator.
+    A LLM data generator can accept prompts and generate synthetic Text2SQL dataset.
+    """
+
+    @abstractmethod
+    def generate_synthetic_dataset(self):
+        """Function for generating synthetic dataset"""
+        pass
+
+    @abstractmethod
+    def _chat_llm(self):
+        """Function for interacting with LLMs"""
+        pass
+
+    @abstractmethod
+    def _writeout_dataset(self):
+        """Function for writing out generated dataset"""
+        pass

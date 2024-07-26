0c668c5871135fe0bd4b6f7af94d1fee787a3b26
yeounoh <yeounoh@google.com>
2024-07-26 13:12:38 -0700

    parallelize inference

--- dbgpt_hub/predict/predict.py
+++ dbgpt_hub/predict/predict.py
@@ -9,6 +9,7 @@ sys.path.append(ROOT_PATH)
 
 from tqdm import tqdm
 from typing import List, Dict, Optional, Any
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 from dbgpt_hub.data_process.data_utils import extract_sql_prompt_dataset
 from dbgpt_hub.llm_base.chat_model import ChatModel
@@ -24,6 +25,35 @@ def prepare_dataset(predict_file_path: Optional[str] = None, ) -> List[Dict]:
     predict_data = [extract_sql_prompt_dataset(item) for item in data]
     return predict_data
 
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from tqdm import tqdm
+
+def inference_worker(model, item, input_kwargs):  # Worker function for a single inference task
+    n_candidates = 1
+    cands = []
+    for i in range(n_candidates):
+        response, _ = model.chat(query=item["input"], history=[], **input_kwargs)
+        response = model.verify_and_correct(item["input"], response, db_folder_path)
+        cands.append(response)
+    if n_candidates == 1:
+        return response
+    else:
+        query = item["input"].split('Also consider the "Rules" and some useful "Hints" if provided.')[1].split('Now generate SQLite SQL query to answer the given "Question".')[0]
+        return model.majority_voting(query, cands)
+
+def parallelized_inference(model: ChatModel, predict_data: List[Dict], **input_kwargs):
+    num_threads = 10  # Set the desired number of threads
+    res = []
+    with ThreadPoolExecutor(max_workers=num_threads) as executor:
+        # Submit all inference tasks to the executor
+        futures = [executor.submit(inference_worker, model, item, input_kwargs) for item in predict_data]
+
+        with tqdm(total=len(predict_data), desc="Inference Progress", unit="item") as pbar:
+            for future in as_completed(futures):
+                # Gather results as they become available and update progress
+                res.append(future.result())
+                pbar.update(1)
+    return res
 
 def inference(model: ChatModel, predict_data: List[Dict], **input_kwargs):
     res = []
@@ -52,7 +82,7 @@ def predict(model: ChatModel):
     args = model.data_args
     ## predict file can be give by param --predicted_input_filename ,output_file can be gived by param predicted_out_filename
     predict_data = prepare_dataset(args.predicted_input_filename)
-    result = inference(model, predict_data)
+    result = parallelized_inference(model, predict_data)
 
     with open(args.predicted_out_filename, "w") as f:
         for p in result:

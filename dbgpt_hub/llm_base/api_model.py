04472666e185caa8b063027555c37b361aabafee
yeounoh <yeounoh@google.com>
2024-07-26 16:39:24 -0700

    flash quota is small so do not use it

--- dbgpt_hub/llm_base/api_model.py
+++ dbgpt_hub/llm_base/api_model.py
@@ -119,7 +119,7 @@ class GeminiModel:
                                            ):query.find("###Question###")]
             input_str = query[query.find("###Question###"):query.find(
                 "Now generate SQLite SQL query to answer the given")]
-            _sql = self._generate_sql(NOT_NULL_TEMPLATE.format(sql=s, question=input_str), use_flash=True)
+            _sql = self._generate_sql(NOT_NULL_TEMPLATE.format(sql=s, question=input_str), use_flash=False)
             _sql = self._generate_sql(SELECT_FIX_TEMPLATE.format(sql=_sql, question=input_str, schema=context_str))
             return _sql
 
